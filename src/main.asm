58ba4cd79104a2e95334f1b7ef74166c1559eab8
ylf <2872444509@qq.com>
2021-10-19 02:15:18 +0800

    尝试加背景图片

--- src/main.asm
+++ src/main.asm
@@ -19,7 +19,11 @@ includelib	kernel32.lib
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; EQU 等值段
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; ico
 ICO_GAME	equ	1000
+
+; Bitmap
+HOME_PAGE	equ 100
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; 数据段
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -27,18 +31,73 @@ ICO_GAME	equ	1000
 hInstance	dd		?
 hWinMain	dd		?
 
+.data
+_page 		dword	100
+
 .const
-szClassName	db	'MyClass',0
-szCaptionMain	db	'MUG',0
-szText		db	'TODO',0
+szClassName		db	'MUG GAME', 0
+szCaptionMain	db	'MUG', 0
+szText			db	'TODO', 0
+WINDOW_HEIGHT 	dword 640
+WINDOW_WIDTH  	dword 480
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; 代码段
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 .code
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; 函数声明
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; 画自定义背景
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_DrawCustomizedBackground	proc _hDC
+		local @hDcBack; 'Back' for 'background'. 
+		;demo: How to index an array.
+		;mov		eax,	1
+		;mov		ecx,	type NetworkMsg
+		;mul		ecx
+		;mov		inputQueue.msgs[eax].sender, 233
+		invoke	CreateCompatibleDC,_hDC; 创建与_hDC兼容的另一个DC(设备上下文)，以备后续操作
+		mov		@hDcBack, eax
+		.if	_page == HOME_PAGE
+			invoke LoadBitmap, 0, HOME_PAGE
+			invoke	SelectObject, @hDcBack, eax
+;		.elseif _page == HOME_MULTIPLE_PAGE
+;			invoke	SelectObject, @hDcBack, _bg2
+		.endif
+		invoke	BitBlt,_hDC,0,0,WINDOW_WIDTH, WINDOW_HEIGHT, @hDcBack,0,0,SRCCOPY
+;		invoke	DeleteDC, @hDcBack ;回收资源（DC）
+		ret
+_DrawCustomizedBackground	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; _Onpaint
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_OnPaint	proc	_hWnd,_hDC
+		local	@stTime:SYSTEMTIME, @bufferDC; bufferDC is cache for pictures.
+		local	@bufferBmp
+		local	@i, @j
+		pushad
+;		invoke	GetLocalTime,addr @stTime
+;		invoke	_CalcClockParam
+;********************************************************************
+; 启用双缓冲绘图方式，避免界面闪烁
+;********************************************************************
+		invoke	CreateCompatibleDC, _hDC
+		mov		@bufferDC,	eax
+		invoke	CreateCompatibleBitmap, _hDC, WINDOW_WIDTH, WINDOW_HEIGHT
+		mov		@bufferBmp,	eax
+		invoke	SelectObject, @bufferDC, @bufferBmp
+;********************************************************************
+; 画自定义背景
+;********************************************************************
+		invoke _DrawCustomizedBackground, @bufferDC
+
+		ret
+_OnPaint endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; 窗口过程
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-_ProcWinMain	proc	uses ebx edi esi,hWnd,uMsg,wParam,lParam
+_ProcWinMain	proc	uses ebx edi esi, hWnd, uMsg, wParam, lParam
 		local	@stPs:PAINTSTRUCT
 		local	@stRect:RECT
 		local	@hDc
@@ -46,26 +105,21 @@ _ProcWinMain	proc	uses ebx edi esi,hWnd,uMsg,wParam,lParam
 		mov	eax,uMsg
 ;********************************************************************
 		.if	eax ==	WM_PAINT
-			invoke	BeginPaint,hWnd,addr @stPs
-			mov	@hDc,eax
-
-			invoke	GetClientRect,hWnd,addr @stRect
-			invoke	DrawText,@hDc,addr szText,-1,\
-				addr @stRect,\
-				DT_SINGLELINE or DT_CENTER or DT_VCENTER
-
-			invoke	EndPaint,hWnd,addr @stPs
+			invoke	BeginPaint, hWnd, addr @stPs
+			mov	@hDc, eax
+			invoke _OnPaint, hWnd, @hDc 
+			invoke	EndPaint, hWnd, addr @stPs
 ;********************************************************************
 		.elseif	eax ==	WM_CLOSE
-			invoke	DestroyWindow,hWinMain
-			invoke	PostQuitMessage,NULL
+			invoke	DestroyWindow, hWinMain
+			invoke	PostQuitMessage, NULL
 ;********************************************************************
 		.else
-			invoke	DefWindowProc,hWnd,uMsg,wParam,lParam
+			invoke	DefWindowProc, hWnd, uMsg, wParam, lParam
 			ret
 		.endif
 ;********************************************************************
-		xor	eax,eax
+		xor	eax, eax
 		ret
 
 _ProcWinMain	endp
@@ -74,44 +128,44 @@ _WinMain	proc
 		local	@stWndClass:WNDCLASSEX
 		local	@stMsg:MSG
 
-		invoke	GetModuleHandle,NULL
-		mov	hInstance,eax
-		invoke	RtlZeroMemory,addr @stWndClass,sizeof @stWndClass
+		invoke	GetModuleHandle, NULL
+		mov	hInstance, eax
+		invoke	RtlZeroMemory, addr @stWndClass, sizeof @stWndClass
 ;********************************************************************
 ; 注册窗口类
 ;********************************************************************
-		invoke	LoadCursor,0,IDC_ARROW
-		mov	@stWndClass.hCursor,eax
+		invoke	LoadCursor, 0, IDC_ARROW
+		mov	@stWndClass.hCursor, eax
 		push	hInstance
 		pop	@stWndClass.hInstance
-		mov	@stWndClass.cbSize,sizeof WNDCLASSEX
-		mov	@stWndClass.style,CS_HREDRAW or CS_VREDRAW
-		mov	@stWndClass.lpfnWndProc,offset _ProcWinMain
-		mov	@stWndClass.hbrBackground,COLOR_WINDOW + 1
-		mov	@stWndClass.lpszClassName,offset szClassName
+		mov	@stWndClass.cbSize, sizeof WNDCLASSEX
+		mov	@stWndClass.style, CS_HREDRAW or CS_VREDRAW
+		mov	@stWndClass.lpfnWndProc, offset _ProcWinMain
+		mov	@stWndClass.hbrBackground, COLOR_WINDOW + 1
+		mov	@stWndClass.lpszClassName, offset szClassName
 ;		加载图标句柄
-		invoke LoadCursor,0,ICO_GAME
+		invoke LoadCursor, 0, ICO_GAME
 		mov @stWndClass.hIcon, eax
 		mov @stWndClass.hIconSm, eax
-		invoke	RegisterClassEx,addr @stWndClass
+		invoke	RegisterClassEx, addr @stWndClass
 ;********************************************************************
 ; 建立并显示窗口
 ;********************************************************************
-		invoke	CreateWindowEx,WS_EX_CLIENTEDGE,offset szClassName,offset szCaptionMain,\
+		invoke	CreateWindowEx, WS_EX_CLIENTEDGE, offset szClassName, offset szCaptionMain,\
 			WS_OVERLAPPEDWINDOW,\
-			100,100,640,480,\
-			NULL,NULL,hInstance,NULL
-		mov	hWinMain,eax
-		invoke	ShowWindow,hWinMain,SW_SHOWNORMAL
-		invoke	UpdateWindow,hWinMain
+			100, 100, 640, 480,\
+			NULL, NULL, hInstance, NULL
+		mov	hWinMain, eax
+		invoke	ShowWindow, hWinMain, SW_SHOWNORMAL
+		invoke	UpdateWindow, hWinMain
 ;********************************************************************
 ; 消息循环
 ;********************************************************************
 		.while	TRUE
-			invoke	GetMessage,addr @stMsg,NULL,0,0
+			invoke	GetMessage, addr @stMsg, NULL, 0, 0
 			.break	.if eax	== 0
-			invoke	TranslateMessage,addr @stMsg
-			invoke	DispatchMessage,addr @stMsg
+			invoke	TranslateMessage, addr @stMsg
+			invoke	DispatchMessage, addr @stMsg
 		.endw
 		ret
 
@@ -119,6 +173,6 @@ _WinMain	endp
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 start:
 		call	_WinMain
-		invoke	ExitProcess,NULL
+		invoke	ExitProcess, NULL
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 end	start
